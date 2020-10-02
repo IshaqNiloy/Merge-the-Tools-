0c860920b8e89dde39d576acd8b53780f2c47cca
Ishaq Ali <ishaqniloy1996@gmail.com>
2020-10-02 03:42:49 -0700

    Added documentation

--- main (1).py
+++ main (1).py
@@ -4,6 +4,8 @@ def merge_the_tools(string, k):
     my_list = []
     my_list = re.findall('[A-Z]{0,%s}' %k, string)
     for i in range(len(my_list) - 1):
+        #Converting the string to a set to keep only the distinct characters.
+        #The second parameter of the sorted function maintains the order of characters in the list as it was in the initial input
         s = sorted(set(my_list[i]), key = my_list[i].index)
         s = ''.join(s)
         print(s)
